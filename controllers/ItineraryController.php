e507021cd3ec48dcb86bc46462196a6802c10a0d
arnaud <collectifCemTwitter@gmail.com>
2025-01-19 19:32:16 +0100

    Première version

--- controllers/ItineraryController.php
+++ controllers/ItineraryController.php
@@ -3,7 +3,7 @@ require_once  __DIR__ . '/../models/OTPModel.php';
 
 class ItineraryController {
     public static function showForm() {
-        include 'views/form.php';
+        include  __DIR__ . '/../views/form.php';
     }
 
     public static function calculateItinerary() {
@@ -20,6 +20,6 @@ class ItineraryController {
         $model = new OTPModel();
         $tripPatterns = $model->getWalkingTrip($fromLat, $fromLng, $toLat, $toLng);
 
-        include 'views/result.php';
+        include __DIR__ . '/../views/result.php';
     }
 }
