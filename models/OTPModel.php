dd533a3d0250d2ecd04292301e2387dbe324a0ec
arnaud <collectifCemTwitter@gmail.com>
2025-01-20 10:06:40 +0100

    Première version

--- models/OTPModel.php
+++ models/OTPModel.php
@@ -11,22 +11,81 @@ class OTPModel {
     public function getWalkingTrip($fromLat, $fromLng, $toLat, $toLng) {
         // Construire la requête GraphQL
         $query = <<<GRAPHQL
-        query trip(\$from: InputCoordinates!, \$to: InputCoordinates!, \$dateTime: DateTime!) {
-          plan(
-            from: \$from
-            to: \$to
-            dateTime: \$dateTime
-            modes: ["WALK"]
+        query trip($from: Location!, $to: Location!, $arriveBy: Boolean, $dateTime: DateTime, $numTripPatterns: Int, $searchWindow: Int, $modes: Modes, $itineraryFiltersDebug: ItineraryFilterDebugProfile, $wheelchairAccessible: Boolean, $pageCursor: String) {
+          trip(
+            from: $from
+            to: $to
+            arriveBy: $arriveBy
+            dateTime: $dateTime
+            numTripPatterns: $numTripPatterns
+            searchWindow: $searchWindow
+            modes: $modes
+            itineraryFilters: {debug: $itineraryFiltersDebug}
+            wheelchairAccessible: $wheelchairAccessible
+            pageCursor: $pageCursor
           ) {
-            itineraries {
+            previousPageCursor
+            nextPageCursor
+            tripPatterns {
+              aimedStartTime
+              aimedEndTime
+              expectedEndTime
+              expectedStartTime
               duration
               distance
+              generalizedCost
               legs {
+                id
                 mode
+                aimedStartTime
+                aimedEndTime
+                expectedEndTime
+                expectedStartTime
+                realtime
                 distance
                 duration
-                from { name }
-                to { name }
+                generalizedCost
+                fromPlace {
+                  name
+                  quay {
+                    id
+                  }
+                }
+                toPlace {
+                  name
+                  quay {
+                    id
+                  }
+                }
+                toEstimatedCall {
+                  destinationDisplay {
+                    frontText
+                  }
+                }
+                line {
+                  publicCode
+                  name
+                  id
+                  presentation {
+                    colour
+                  }
+                }
+                authority {
+                  name
+                  id
+                }
+                pointsOnLink {
+                  points
+                }
+                interchangeTo {
+                  staySeated
+                }
+                interchangeFrom {
+                  staySeated
+                }
+              }
+              systemNotices {
+                tag
               }
             }
           }
